2b21ef092406aa8ca7fe9b2a3e13e0ab968dfa5d
profPlantBoy <nickolaskeefer@gmail.com>
2025-09-02 18:42:46 -0400

    Setup Files

---
+++ src/main/java/net/profplanyboy/sensorywizards2/SensoryWizards2DataGenerator.java
@@ -0,0 +1,11 @@
+package net.profplanyboy.sensorywizards2;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SensoryWizards2DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/profplanyboy/sensorywizards2/client/SensoryWizards2Client.java
@@ -0,0 +1,12 @@
+package net.profplanyboy.sensorywizards2.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SensoryWizards2Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+// On Initialize Client Section
+
+
+    }
+}
